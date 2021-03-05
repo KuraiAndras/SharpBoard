90a93d1f6cab60e47898091eddf02823790925b8
Kurai András <k.andras9642@gmail.com>
2021-03-05 23:59:19 +0100

    check formatting in ci

---
+++ SharpBoard.Build/Formatting.cs
@@ -0,0 +1,18 @@
+﻿using Nuke.Common;
+using Nuke.Common.Tooling;
+
+// ReSharper disable InconsistentNaming
+
+sealed partial class Build
+{
+    [PackageExecutable(
+        "dotnet-format",
+        "dotnet-format.dll")]
+    readonly Tool DotnetFormat = default!;
+
+    Target CheckFormat => _ => _
+        .Executes(() => DotnetFormat("--check"));
+
+    Target Format => _ => _
+        .Executes(() => DotnetFormat());
+}
