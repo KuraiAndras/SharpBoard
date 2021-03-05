cd6bc853af13c7c62a3f76e8fc8991f160165487
Kurai András <k.andras9642@gmail.com>
2021-03-05 20:55:38 +0100

    add nuke build

---
+++ SharpBoard.Build/Build.cs
@@ -0,0 +1,43 @@
+﻿using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[ShutdownDotNetAfterServerBuild]
+class Build : NukeBuild
+{
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution = default!;
+    [GitVersion] readonly GitVersion GitVersion = default!;
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    Target Clean => _ => _
+        .Executes(() => EnsureCleanDirectory(ArtifactsDirectory));
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() => DotNetRestore(s => s
+            .SetProjectFile(Solution)));
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() => DotNetBuild(s => s
+            .SetProjectFile(Solution)
+            .SetConfiguration(Configuration)
+            .SetAssemblyVersion(GitVersion.AssemblySemVer)
+            .SetFileVersion(GitVersion.AssemblySemFileVer)
+            .SetInformationalVersion(GitVersion.InformationalVersion)
+            .EnableNoRestore()));
+
+}
