a6434df220fcc72d6ff21b38fa2432147eef4c98
Kurai András <k.andras9642@gmail.com>
2021-03-06 12:19:25 +0100

    use nuke for running nukeeper

---
+++ SharpBoard.Build/Nukeeper.cs
@@ -0,0 +1,19 @@
+﻿// ReSharper disable InconsistentNaming
+
+using Nuke.Common;
+using Nuke.Common.Tooling;
+
+sealed partial class Build
+{
+    [Parameter] readonly string NukeeperToken = string.Empty;
+
+    [PackageExecutable(
+        "nukeeper",
+        "nukeeper.dll",
+        Framework = "netcoreapp3.1")]
+    readonly Tool Nukeeper = default!;
+
+    Target UpdatePackages => _ => _
+        .Executes(() => Nukeeper(
+            $"repo {Repository.HttpsUrl} {NukeeperToken} -a 0 --targetBranch develop --maxpackageupdates 100 --consolidate"));
+}
