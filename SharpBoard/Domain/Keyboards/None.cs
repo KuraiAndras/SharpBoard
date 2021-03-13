8ab620b37a17ebfe0410f0e2a5b7eb20b0b13c3a
Kurai András <k.andras9642@gmail.com>
2021-03-13 20:28:17 +0100

    add logging to services

--- SharpBoard/Domain/Keyboards/None.cs
+++ SharpBoard/Domain/Keyboards/None.cs
@@ -1,10 +1,20 @@
-﻿using System.Threading;
+﻿using Microsoft.Extensions.Logging;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SharpBoard.Domain.Keyboards
 {
     public sealed class None : IKeyBoard
     {
-        public Task SetColorValue(ColorRgb256 color, int keyId, CancellationToken cancellationToken = default) => Task.CompletedTask;
+        private readonly ILogger<None> _logger;
+
+        public None(ILogger<None> logger) => _logger = logger;
+
+        public Task SetColorValue(ColorRgb256 color, int keyId, CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation("Pressed key {KeyId} with color {Color}", keyId, color);
+
+            return Task.CompletedTask;
+        }
     }
 }
--- SharpBoard/Domain/Keyboards/Redragon.cs
+++ SharpBoard/Domain/Keyboards/Redragon.cs
@@ -1,12 +1,19 @@
-﻿using System.Threading;
+﻿using Microsoft.Extensions.Logging;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SharpBoard.Domain.Keyboards
 {
     public sealed class Redragon : IKeyBoard
     {
+        private readonly ILogger<Redragon> _logger;
+
+        public Redragon(ILogger<Redragon> logger) => _logger = logger;
+
         public Task SetColorValue(ColorRgb256 color, int keyId, CancellationToken cancellationToken = default)
         {
+            _logger.LogError("Pressed key {KeyId} with color {Color} is not implemented", keyId, color);
+
             // TODO: Implement this
 
             return Task.CompletedTask;
